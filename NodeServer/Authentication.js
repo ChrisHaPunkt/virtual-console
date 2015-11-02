5f794c7c7736ff9f8e864d3cfff0de3cae2b35d9
Dennis Hofmann <you@example.com>
2015-11-02 21:32:48 +0100

    Add authentication prototyp

--- NodeServer/Authentication.js
+++ NodeServer/Authentication.js
@@ -2,6 +2,17 @@
  * Created by dennis on 29.10.15.
  */
 
+var database = require("Database.js");
 
+function authentication(){
+    this.database = new database("mongodb://localhost:27017/M113");
 
-
+    publicSection = {
+        check:function(userName, userPass){
+            query = { "name":user, "password":userPass};
+            onSuccess = function(){console.log("Login successfuly");};
+            database.query("userData", query, onSuccess);
+        }
+    };
+    return publicSection;
+}
