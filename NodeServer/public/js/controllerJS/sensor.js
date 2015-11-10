c077a8459dd031f909a78ca9d3a828f51873cd4f
Ron <SchiwkowskiR@gmx.net>
2015-11-10 15:15:05 +0100

    Added motion-class

--- NodeServer/public/js/controllerJS/sensor.js
+++ NodeServer/public/js/controllerJS/sensor.js
@@ -7,54 +7,102 @@
  */
 
 /*
-function getBrowserByFeature(){
-var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-// Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
-var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
-var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-// At least Safari 3+: "[object HTMLElementConstructor]"
-var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
-var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6}
+ function getBrowserByFeature(){
+ var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+ // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+ var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+ var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+ // At least Safari 3+: "[object HTMLElementConstructor]"
+ var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+ var isIE = /*@cc_on!@*/
+false || !!document.documentMode; // At least IE6}
 
-function getMotionData(rnd){
-    window.ondevicemotion = function(event) {
+
+/*window.ondevicemotion = function  (event) {
+    var accelerationX = event.acceleration.x; //hoch, runter
+    var accelerationY = event.acceleration.y; //hoch, runter
+    var accelerationZ = event.acceleration.z; //vor, zurück
+
+    var rotationAlpha = event.rotationRate.alpha; //rotation um karthesische X-Achse (kippen vor, zurück)
+    var rotationBeta = event.rotationRate.beta; //rotation um karthesische Y-Achse (drehen)
+    var rotationGamma = event.rotationRate.gamma; //rotation um karthesische Z-Achse (kippen links, rechts)
+
+    var interval = event.interval;
+
+    var MotionData ={
+        accelerationX:accelerationX,
+        accelerationY:accelerationY,
+        accelerationZ:accelerationZ,
+        rotationAlpha:rotationAlpha,
+        rotationBeta:rotationBeta,
+        rotationGamma:rotationGamma
+    };
+    socket.sendData(MotionData);
+}
+*/
+/*
+function startMotionCapture(){
+    window.addEventListener('devicemotion', function onDeviceMoved(event){
+    //window.ondevicemotion = function  (event) {
         var accelerationX = event.acceleration.x; //hoch, runter
         var accelerationY = event.acceleration.y; //hoch, runter
         var accelerationZ = event.acceleration.z; //vor, zurück
 
-        var rotationAlpha    = event.rotationRate.alpha; //rotation um karthesische X-Achse (kippen vor, zurück)
-        var rotationBeta   = event.rotationRate.beta; //rotation um karthesische Y-Achse (drehen)
-        var rotationGamma   = event.rotationRate.gamma; //rotation um karthesische Z-Achse (kippen links, rechts)
+        var rotationAlpha = event.rotationRate.alpha; //rotation um karthesische X-Achse (kippen vor, zurück)
+        var rotationBeta = event.rotationRate.beta; //rotation um karthesische Y-Achse (drehen)
+        var rotationGamma = event.rotationRate.gamma; //rotation um karthesische Z-Achse (kippen links, rechts)
 
         var interval = event.interval;
 
-        if(typeof(rnd) != "undefined"){
-            accelerationX = accelerationX.toFixed(rnd);
-            accelerationY = accelerationY.toFixed(rnd);
-            accelerationZ = accelerationZ.toFixed(rnd);
-            rotationAlpha = rotationAlpha.toFixed(rnd);
-            rotationBeta = rotationBeta.toFixed(rnd);
-            rotationGamma =rotationGamma.toFixed(rnd);
-        }
-
-        var motion = {
-            accelerationX: accelerationX,
-            accelerationY: accelerationY,
-            accelerationZ: accelerationZ,
-            rotationAlpha: rotationAlpha,
-            rotationBeta: rotationBeta,
-            rotationGamma: rotationGamma,
-            interval: interval
-        }
-        return motion;
-    }
+        var MotionData ={
+            accelerationX:accelerationX,
+            accelerationY:accelerationY,
+            accelerationZ:accelerationZ,
+            rotationAlpha:rotationAlpha,
+            rotationBeta:rotationBeta,
+            rotationGamma:rotationGamma
+        };
+        socket.sendData(MotionData.accelerationX);
+    });
 }
+startMotionCapture();
+*/
 
-function vibrate(milliseconds){
+
+
+
+
+function startMotionCapture(){
+    window.addEventListener('devicemotion', function  (event){
+        //window.ondevicemotion = function  (event) {
+        var accelerationX = event.acceleration.x; //hoch, runter
+        var accelerationY = event.acceleration.y; //hoch, runter
+        var accelerationZ = event.acceleration.z; //vor, zurück
+
+        var rotationAlpha = event.rotationRate.alpha; //rotation um karthesische X-Achse (kippen vor, zurück)
+        var rotationBeta = event.rotationRate.beta; //rotation um karthesische Y-Achse (drehen)
+        var rotationGamma = event.rotationRate.gamma; //rotation um karthesische Z-Achse (kippen links, rechts)
+
+        var interval = event.interval;
+
+        var MotionData ={
+            accelerationX:accelerationX,
+            accelerationY:accelerationY,
+            accelerationZ:accelerationZ,
+            rotationAlpha:rotationAlpha,
+            rotationBeta:rotationBeta,
+            rotationGamma:rotationGamma
+        };
+        socket.sendData(MotionData);
+    });
+}
+startMotionCapture();
+
+function vibrate(milliseconds) {
     window.navigator.vibrate(milliseconds);
 }
 
-function getUserAudioVideo(){
+function getUserAudioVideo() {
     /*
      VIDEO AND AUDIO CAPTURING WITH getUserMedia()
      */
@@ -74,7 +122,7 @@ function getUserAudioVideo(){
                 video: true
             },
             // Success Callback
-            function(localMediaStream) {
+            function (localMediaStream) {
                 // Get a reference to the video element on the page.
                 var vid = document.getElementById('camera-stream');
                 // Create an object URL for the video stream and use this
@@ -82,7 +130,7 @@ function getUserAudioVideo(){
                 vid.src = window.URL.createObjectURL(localMediaStream);
             },
             // Error Callback
-            function(err) {
+            function (err) {
                 // Log the error to the console.
                 alert('The following error occurred when trying to use getUserMedia: ' + err);
             }
@@ -92,55 +140,30 @@ function getUserAudioVideo(){
     }
 }
 
-function getBatteryStatus() {
-    var BatteryStatus = {
-        batteryChargingState: batteryChargingState,
-        batteryLevel: batteryLevel,
-        batteryChargingTime:batteryChargingTime,
-        batteryDischargingTime:batteryDischargingTime
-    }
-    navigator.getBattery().then(function (battery) {
+navigator.getBattery().then(function (battery) {
+    var batteryChargingState = battery.charging;
+    var batteryLevel = battery.level;
+    var batteryChargingTime = battery.chargingTime;
+    var batteryDischargingTime = battery.dischargingTime;
 
-            var batteryChargingState = battery.charging;
-            var batteryLevel = battery.level;
-            var batteryChargingTime = battery.chargingTime;
-            var batteryDischargingTime = battery.dischargingTime;
-        //EVENTS
-           /* battery.addEventListener('chargingchange', function () {
-                batteryChargingState = battery.charging;
-            });
-            battery.addEventListener('levelchange', function () {
-                batteryLevel = battery.level;
-            });
-            battery.addEventListener('chargingtimechange', function () {
-                batteryChargingTime = battery.chargingTime;
-            });
-            battery.addEventListener('dischargingtimechange', function () {
-                batteryDischargingTime = battery.dischargingTime;
-            });*/
-        BatteryStatus.batteryChargingState= batteryChargingState;
-        BatteryStatus.batteryLevel= batteryLevel;
-        BatteryStatus.batteryChargingTime= batteryChargingTime;
-        BatteryStatus.batteryDischargingTime= batteryDischargingTime;
-
-        return BatteryStatus;
-    });
-    return BatteryStatus;
-}
-
-var battery = getBatteryStatus()
-$('#batteryChargingState').html("Battery charging? " + (battery.batteryChargingState));
-$('#batteryLevel').html("Battery level: " + battery.batteryLevel * 100 + "%");
-$('#batteryChargingTime').html("Battery charging time: " + battery.batteryChargingTime + " seconds");
-$('#batteryDischargingTime').html("Battery discharging time: " + battery.batteryDischargingTime + " seconds");
+     battery.addEventListener('chargingchange', function () {
+     batteryChargingState = battery.charging;
+     });
+     battery.addEventListener('levelchange', function () {
+     batteryLevel = battery.level;
+     });
+     battery.addEventListener('chargingtimechange', function () {
+     batteryChargingTime = battery.chargingTime;
+     });
+     battery.addEventListener('dischargingtimechange', function () {
+     batteryDischargingTime = battery.dischargingTime;
+     });
+});
 
 
-function getDeviceOrientation(){
+function getDeviceOrientation() {
     var deviceOrientation = window.orientation
     var isPortrait = deviceOrientation % 180 === 0;
     document.body.className = isPortrait ? 'portrait' : 'landscape';
     return document.body.className;
 }
-
- var Orientation = getDeviceOrientation();
-$('#x').html("Orientation: " + (Orientation));
