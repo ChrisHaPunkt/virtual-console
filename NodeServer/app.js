32a8a3ffea4f7d7651c954aed3221b5c2bccc866
Chris <chris@homeset.de>
2015-11-26 17:44:39 +0100

    Button Container, rename frontend -> game

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/login', loginSite);
 app.use('/users', users);
-app.use('/frontend', frontendRoute);
+app.use('/game', frontendRoute);
 app.use('/controller', controllerDemo);
 
 // catch 404 and forward to error handler
