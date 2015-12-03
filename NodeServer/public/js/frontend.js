e35702cf822cc30dadfdedd1703a374fc7dcc7eb
hannes <hannes.grothkopf@student.fh-kiel.de>
2015-12-03 18:53:03 +0100

    made frontend.js entry point of require.js app

--- NodeServer/public/js/frontend.js
+++ NodeServer/public/js/frontend.js
@@ -1,33 +1,45 @@
-window.onload = function () {
-
-    var serverUrl = 'http://149.222.137.126';
-    var serverPort = '5222';
-
-    var socket = io.connect();//serverUrl + ':' + serverPort);
-
-    socket.on('connect', function () {
-        socket.emit('frontendInit', 'hi');
-    });
-
-    socket.on('frontendData', function (data) {
-        console.log("on.FrontendData: ", data);
-        writeContent('client ' + data.clientId + ': ' + data.message);
-    });
-    socket.on('frontendMessage', function (data) {
-        console.log("on.FrontendMessage: ", data);
-
-        writeContent('client ' + data.data.clientName + ': ' + data.data.message);
-
-
-        if (data.type == "button") {
-
-            var a = 200 * Math.random();
-            var b = 200 * Math.random();
-            var c = 200 * Math.random();
-
-            GameHandler.adjustCubeSize(0, {x: a, y: b, z: c});
+/**
+ * REQUIRE.JS CONFIG
+ * This is necessary due to problems with the jquery and three js libraries.
+ * See links below for further details.
+ * */
+// http://requirejs.org/docs/jquery.html#noconflictmap
+define("jquery", [], function () {
+    return jQuery.noConflict();
+});
+// http://requirejs.org/docs/api.html#config-shim
+requirejs.config({
+    paths: {
+        "three": "libs/three"
+    },
+    shim: {
+        three: {
+            exports: 'THREE'
         }
-    });
+    }
+});
+
+/**
+ * START OF THE FRONTEND APPLICATION
+ * */
+require(["frontendNetwork", "3dgame"], function (network, game) {
+
+    network.initNetwork({
+        onMessage: function (data) {
+            console.log("Message from Sever: ", data);
+
+            writeContent('client ' + data.data.clientName + ': ' + data.data.message);
+
+            if (data.type == "button") {
+
+                var a = 200 * Math.random();
+                var b = 200 * Math.random();
+                var c = 200 * Math.random();
+
+                game.adjustCubeSize(0, {x: a, y: b, z: c});
+            }
+        }
+    }).start();
 
     // write to content
     var pageContent = document.getElementById('content');
@@ -37,5 +49,4 @@ window.onload = function () {
         }
     };
 
-}();
-
+});
