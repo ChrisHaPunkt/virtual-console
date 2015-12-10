b6be11c998d6c29f87a27ecca8d74761758f9f38
Ron <SchiwkowskiR@gmx.net>
2015-12-10 10:42:28 +0100

    second Template created

--- NodeServer/public/js/controllerJS/sensor.js
+++ NodeServer/public/js/controllerJS/sensor.js
@@ -6,43 +6,159 @@
 
  */
 
-function getMotionData(rnd){
 
-    window.ondevicemotion = function(event) {
-        var accelerationX = event.acceleration.x; //links,rechts
+
+function startMotionCapture(minMotion, minRotation){
+    window.addEventListener('devicemotion', function  (event){
+        //window.ondevicemotion = function  (event) {
+        var accelerationX = event.acceleration.x; //hoch, runter
         var accelerationY = event.acceleration.y; //hoch, runter
         var accelerationZ = event.acceleration.z; //vor, zurück
 
-        var rotationAlpha    = event.rotationRate.alpha; //rotation um karthesische X-Achse (kippen vor, zurück)
-        var rotationBeta   = event.rotationRate.beta; //rotation um karthesische Y-Achse (drehen)
-        var rotationGamma   = event.rotationRate.gamma; //rotation um karthesische Z-Achse (kippen links, rechts)
+        if(accelerationX == null)
+            {accelerationX = 0;}
+        if (accelerationY == null)
+            {accelerationY = 0;}
+        if (accelerationZ == null)
+         {accelerationZ = 0;}
+
+
+        var rotationAlpha = event.rotationRate.alpha; //rotation um karthesische X-Achse (kippen vor, zurück)
+        var rotationBeta = event.rotationRate.beta; //rotation um karthesische Y-Achse (drehen)
+        var rotationGamma = event.rotationRate.gamma; //rotation um karthesische Z-Achse (kippen links, rechts)
+
+        if (rotationAlpha == null)
+            {rotationAlpha = 0;}
+        if (rotationBeta == null)
+            {rotationBeta = 0;}
+        if (rotationGamma == null)
+            {rotationGamma = 0;}
 
         var interval = event.interval;
 
-        if( rnd != []){
-            accelerationX = Math.round(accelerationX).toFixed(rnd);
-            accelerationY = Math.round(accelerationY).toFixed(rnd);
-            accelerationZ = Math.round(accelerationZ).toFixed(rnd);
-            rotationAlpha = Math.round(rotationAlpha).toFixed(rnd);
-            rotationBeta = Math.round(rotationBeta).toFixed(rnd);
-            rotationGamma = Math.round(rotationGamma).toFixed(rnd);
-        }
-        else {
-            rotationAlpha = Math.round(rotationAlpha);
-            rotationBeta = Math.round(rotationBeta);
-            rotationGamma = Math.round(rotationGamma);
-        }
+        var MotionData ={
 
-
-        var motion = {
-            accelerationX: accelerationX,
-            rotationBeta: accelerationY,
-            rotationGamma: accelerationZ,
-            rotationAlpha: rotationAlpha,
-            rotationBeta: rotationBeta,
-            rotationGamma: rotationGamma,
-            interval: interval
+            accelerationX:accelerationX,
+            accelerationY:accelerationY,
+            accelerationZ:accelerationZ,
+            rotationAlpha:rotationAlpha,
+            rotationBeta:rotationBeta,
+            rotationGamma:rotationGamma
         };
-        return motion;
+/*
+        if (MotionData.accelerationX > minMotion || MotionData.accelerationY > minMotion || MotionData.accelerationZ > minMotion ||
+            MotionData.rotationAlpha > minRotation || MotionData.rotationBeta > minRotation || MotionData.rotationGamma > minRotation) {
+            var checkAcc = $('#chkAcc').prop('checked');
+            if (checkAcc==true) {
+                socket.sendData("motionData", MotionData);
+            }
+        }
+
+
+ */
+         var AccelerationData ={
+         accelerationX:accelerationX,
+         accelerationY:accelerationY,
+         accelerationZ:accelerationZ,
+         };
+         var RotationData ={
+         rotationAlpha:rotationAlpha,
+         rotationBeta:rotationBeta,
+         rotationGamma:rotationGamma
+         };
+
+
+
+
+        if (MotionData.accelerationX > minMotion || MotionData.accelerationY > minMotion || MotionData.accelerationZ > minMotion ||
+            MotionData.accelerationX < -minMotion || MotionData.accelerationY < -minMotion || MotionData.accelerationZ < -minMotion) {
+            var checkAcc = $('#chkAcc').prop('checked');
+            if (checkAcc==true) {
+                socket.sendData("accelerationData",AccelerationData);
+            }
+        }
+        if (MotionData.rotationAlpha > minRotation || MotionData.rotationBeta > minRotation || MotionData.rotationGamma > minRotation ||
+            MotionData.rotationAlpha < -minRotation || MotionData.rotationBeta < -minRotation || MotionData.rotationGamma < -minRotation
+        ) {
+            var checkRot = $('#chkRot').prop('checked');
+            if (checkRot==true) {
+                socket.sendData("rotationData",RotationData);
+            }
+        }
+
+
+    });
+}
+
+
+startMotionCapture(0.3, 6);
+
+function vibrate(milliseconds) {
+    window.navigator.vibrate(milliseconds);
+}
+
+function getUserAudioVideo() {
+    /*
+     VIDEO AND AUDIO CAPTURING WITH getUserMedia()
+     */
+    navigator.getUserMedia = (navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+
+    // Check that the browser supports getUserMedia.
+    // If it doesn't show an alert, otherwise continue.
+    if (navigator.getUserMedia) {
+        // Request the camera.
+        navigator.getUserMedia(
+            // Constraints
+            {
+                audio: true,
+                video: true
+            },
+            // Success Callback
+            function (localMediaStream) {
+                // Get a reference to the video element on the page.
+                var vid = document.getElementById('camera-stream');
+                // Create an object URL for the video stream and use this
+                // to set the video source.
+                vid.src = window.URL.createObjectURL(localMediaStream);
+            },
+            // Error Callback
+            function (err) {
+                // Log the error to the console.
+                alert('The following error occurred when trying to use getUserMedia: ' + err);
+            }
+        );
+    } else {
+        alert('Sorry, your browser does not support getUserMedia');
     }
+}
+
+navigator.getBattery().then(function (battery) {
+    var batteryChargingState = battery.charging;
+    var batteryLevel = battery.level;
+    var batteryChargingTime = battery.chargingTime;
+    var batteryDischargingTime = battery.dischargingTime;
+
+    battery.addEventListener('chargingchange', function () {
+        batteryChargingState = battery.charging;
+    });
+    battery.addEventListener('levelchange', function () {
+        batteryLevel = battery.level;
+    });
+    battery.addEventListener('chargingtimechange', function () {
+        batteryChargingTime = battery.chargingTime;
+    });
+    battery.addEventListener('dischargingtimechange', function () {
+        batteryDischargingTime = battery.dischargingTime;
+    });
+});
+
+
+function getDeviceOrientation() {
+    var deviceOrientation = window.orientation
+    var isPortrait = deviceOrientation % 180 === 0;
+    document.body.className = isPortrait ? 'portrait' : 'landscape';
+    return document.body.className;
 }
