a315985e35920a2a1825670df80bbfef2f3aaa54
Chris <chris@homeset.de>
2015-12-17 15:13:11 +0100

    Merge branch 'master' into GameControllerInterface
    # Conflicts:
    #	NodeServer/public/js/3dgame.js
    #	NodeServer/routes/index.js
    #	NodeServer/views/controller/index.jade

--- NodeServer/routes/index.js
+++ NodeServer/routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('controller/index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var chosenControllerTemplate = require('../app').get('chosenControllerTemplate');
+
+    var gameController = requirejs("../public/js/gameApi").controllerTemplates;
+
+    var controller = null;
+    switch (chosenControllerTemplate) {
+        case gameController.DEMO:
+            controller = "controller/" + "controllerDemo";
+            break;
+        case gameController.MODERN:
+            controller = "controller/" + "controllerModern";
+            break;
+        case gameController.NEW:
+            controller = "controller/" + "controllerMichael";
+            break;
+        default:
+            controller = "controller/" + "controllerModern";
+    }
+
+    res.render(controller, function (err, htmlControllerContent) {
+
+        res.render('controller/index', {
+            title: 'Login',
+            controller: htmlControllerContent
+        });
+
+    });
+
 });
 
 module.exports = router;
