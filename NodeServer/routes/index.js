158d31925bde397427d38bb7f2d3b7e1e50306bb
hannes <hannes.grothkopf@student.fh-kiel.de>
2016-04-05 18:20:30 +0200

    gameController ENUM hardcoded

--- NodeServer/routes/index.js
+++ NodeServer/routes/index.js
@@ -13,7 +13,20 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     var chosenControllerTemplate = require('../app').get('chosenControllerTemplate');
 
-    var gameController = requirejs("../public/js/gameApi").controllerTemplates;
+    var gameController = {
+        DEMO: 4,
+        NEW: 5,
+        MODERN: 6
+    };
+
+    //var gameController = requirejs("../public/js/gameApi").controllerTemplates;
+    /*
+    // http://stackoverflow.com/questions/23179827/node-js-socket-io-socket-io-js-not-found-express-4-0
+    var gameController;
+    requirejs(["../public/js/gameApi"], function(gameApi){
+        gameController = gameApi.controllerTemplates;
+    });
+    */
 
     var controller = null;
     switch (chosenControllerTemplate) {
