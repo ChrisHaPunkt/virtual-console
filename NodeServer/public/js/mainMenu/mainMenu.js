ef54ee37645fb3822c0b08859677c324698b4f3c
hannes <hannes.grothkopf@student.fh-kiel.de>
2016-04-18 17:48:24 +0200

    added custom CSS for mainMenu

--- NodeServer/public/js/mainMenu/mainMenu.js
+++ NodeServer/public/js/mainMenu/mainMenu.js
@@ -22,21 +22,13 @@ define(['jquery'], function ($) {
 
         var init = function (domContainer, gameApi) {
             var apiGameContainer = $('<div/>', {
-                id: 'api_game_container',
-                css: {
-                    'min-height': '200px',
-                    'background-color': 'transparent',
-                    margin: '25px'
-                }
+                id: 'APIgameContainer',
+                class: 'gameContainer'
             }).html('<p>API Games</p>').appendTo(domContainer);
 
             var extGameContainer = $('<div/>', {
-                id: 'ext_game_container',
-                css: {
-                    'min-height': '200px',
-                    'background-color': 'transparent',
-                    margin: '25px'
-                }
+                id: 'EXTgameContainer',
+                class: 'gameContainer'
             }).html('<p>External Games</p>').appendTo(domContainer);
 
             initAPIGameTiles(apiGameContainer);
@@ -47,17 +39,7 @@ define(['jquery'], function ($) {
             for (var i = 0; i < gameInfoData.intGames.length; i++) {
                 $('<div/>', {
                     id: gameInfoData.intGames[i].id + '_tile',
-                    class: 'gameTile',
-                    css: {
-                        width: '100px',
-                        height: '100px',
-                        'background-color': 'black',
-                        float: 'left',
-                        margin: '25px',
-                        color: 'white',
-                        'text-align': 'center',
-                        'line-height': '100px'
-                    },
+                    class: 'gameTile internal',
                     click: function () {
                         window.location = window.location.href.replace('/menu', gameInfoData.intGamesBasePath) + $(this).attr('path');
                     },
@@ -70,17 +52,7 @@ define(['jquery'], function ($) {
             for (var i = 0; i < gameInfoData.extGames.length; i++) {
                 $('<div/>', {
                     id: gameInfoData.extGames[i].id + '_tile',
-                    class: 'gameTile',
-                    css: {
-                        width: '100px',
-                        height: '100px',
-                        'background-color': 'grey',
-                        float: 'left',
-                        margin: '25px',
-                        color: 'white',
-                        'text-align': 'center',
-                        'line-height': '100px'
-                    },
+                    class: 'gameTile external',
                     click: function () {
                         window.location = window.location.href.replace('/menu', gameInfoData.extGamesBasePath) + '?game=' + $(this).attr('path');
                     },
