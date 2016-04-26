7b8784856011f87e6cc2ed07c2269a1605696d38
Chris <chris@homeset.de>
2016-04-26 13:54:14 +0200

    app.sh start | stop implementiert

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 case $1 in
     'start' )
-        mongod --dbpath Database/storage
-
+        mongod --dbpath Database/storage &
+        PID=$!
+        echo ${PID} > scripts/DBPID
         # Restore the data in Database/dumpData in collections named like sub-folders
         # Comment the following line to prevent data restore
-        mongorestore Database\dumpData
+       # mongorestore Database/dumpData
         ;;
     'stop')
-                #  mongod --shutdown funktioniert nicht
+
+            echo "Stopping MongoDB..."
+            kill -INT `cat scripts/DBPID`
+            echo "" > scripts/DBPID
         ;;
 
     *)
