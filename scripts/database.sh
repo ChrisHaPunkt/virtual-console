9f2caedd0f77587f372c0ba51414ad96450ae319
hannes <hannes.grothkopf@student.fh-kiel.de>
2016-04-28 15:36:38 +0200

    updated database startup file

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 case $1 in
     'start' )
-        mongod --dbpath Database/storage &
+        mongod --dbpath ../Database/storage &
         PID=$!
         echo ${PID} > scripts/DBPID
         # Restore the data in Database/dumpData in collections named like sub-folders
