b7531c6e88ebd3bb41101186e9d488d1e75b30ff
Michael Schleiss <michaelschleiss87@gmail.com>
2016-05-10 17:54:48 +0200

    Uinput test5

--- NodeServer/sources/controller2ioctl.js
+++ NodeServer/sources/controller2ioctl.js
@@ -11,11 +11,33 @@ var uinput = require('uinput');
 var setup_options = {
     EV_KEY : [ uinput.KEY_H, uinput.KEY_E, uinput.KEY_L, uinput.KEY_O, uinput.KEY_UP, uinput.KEY_DOWN ]
 }
-exports.buttonEvent = function(buttonName){
-    uinput.key_event(stream, uinput["KEY_" + buttonName], function (err) {
+
+exports.buttonEvent = function(buttonName) {
+    uinput.setup(setup_options, function (err, stream) {
         if (err) {
             throw(err);
         }
+
+        var create_options = {
+            name: 'myuinput',
+            id: {
+                bustype: uinput.BUS_VIRTUAL,
+                vendor: 0x1,
+                product: 0x1,
+                version: 1
+            }
+        };
+
+        uinput.create(stream, create_options, function (err) {
+            if (err) {
+                throw(err);
+            }
+
+            uinput.key_event(stream, uinput["KEY_" + buttonName], function (err) {
+                if (err) {
+                    throw(err);
+                }
+            });
+        });
     });
 };
-console.log('wurst');
