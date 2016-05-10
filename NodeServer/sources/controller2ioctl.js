d0e0f4ceadf7b401b68214e0f01c69476d2762cb
Michael Schleiss <michaelschleiss87@gmail.com>
2016-05-10 18:02:59 +0200

    Uinput test5

--- NodeServer/sources/controller2ioctl.js
+++ NodeServer/sources/controller2ioctl.js
@@ -3,23 +3,17 @@
  */
 var uinput = require('uinput');
 
-/**
- * Module exports.
- */
-
-
 var setup_options = {
-    EV_KEY : [ uinput.KEY_H, uinput.KEY_E, uinput.KEY_L, uinput.KEY_O, uinput.KEY_UP, uinput.KEY_DOWN ]
+    EV_KEY : [ uinput.KEY_H, uinput.KEY_E, uinput.KEY_L, uinput.KEY_O ]
 }
-
-exports.buttonEvent = function(buttonName) {
+var hellotest = function() {
     uinput.setup(setup_options, function (err, stream) {
         if (err) {
             throw(err);
         }
 
         var create_options = {
-            name: 'virtual-gamepad',
+            name: 'myuinput',
             id: {
                 bustype: uinput.BUS_VIRTUAL,
                 vendor: 0x1,
@@ -33,11 +27,38 @@ exports.buttonEvent = function(buttonName) {
                 throw(err);
             }
 
-            uinput.key_event(stream, uinput["KEY_" + buttonName], function (err) {
-                if (err) {
-                    throw(err);
-                }
-            });
+            setTimeout(function () {
+                uinput.key_event(stream, uinput.KEY_H, function (err) {
+                    if (err) {
+                        throw(err);
+                    }
+
+                    uinput.key_event(stream, uinput.KEY_E, function (err) {
+                        if (err) {
+                            throw(err);
+                        }
+
+                        uinput.key_event(stream, uinput.KEY_L, function (err) {
+                            if (err) {
+                                throw(err);
+                            }
+
+                            uinput.key_event(stream, uinput.KEY_L, function (err) {
+                                if (err) {
+                                    throw(err);
+                                }
+
+                                uinput.key_event(stream, uinput.KEY_O, function (err) {
+                                    if (err) {
+                                        throw(err);
+                                    }
+                                });
+                            });
+                        });
+                    });
+                });
+            }, 1000);
         });
     });
 };
+e
