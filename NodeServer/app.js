c7f57bf3163ca47ec6d46234a8e9e946f43dd9f4
Michael Schleiss <michaelschleiss87@gmail.com>
2016-05-10 16:07:46 +0200

    Uinput test

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -8,6 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var util = require('util');
 var System = require('./sources/System');
+var uinput = require('uinput');
 
 var mainMenuRoute = require('./routes/mainMenuRoute');
 var controllerRoutes = require('./routes/controllerRoute');
@@ -22,6 +23,64 @@ var GameVO = require("./sources/ValueObjects/GameVO");
 app.set("fullQualifiedGameVOs", false);
 
 var types = GameHandler.TYPES;
+
+var setup_options = {
+    EV_KEY : [ uinput.KEY_H, uinput.KEY_E, uinput.KEY_L, uinput.KEY_O ]
+}
+
+uinput.setup(setup_options, function(err, stream) {
+    if (err) {
+        throw(err);
+    }
+
+    var create_options = {
+        name : 'myuinput',
+        id : {
+            bustype : uinput.BUS_VIRTUAL,
+            vendor : 0x1,
+            product : 0x1,
+            version : 1
+        }
+    };
+
+    uinput.create(stream, create_options, function(err) {
+        if (err) {
+            throw(err);
+        }
+
+        setTimeout(function() {
+            uinput.key_event(stream, uinput.KEY_H, function(err) {
+                if (err) {
+                    throw(err);
+                }
+
+                uinput.key_event(stream, uinput.KEY_E, function(err) {
+                    if (err) {
+                        throw(err);
+                    }
+
+                    uinput.key_event(stream, uinput.KEY_L, function(err) {
+                        if (err) {
+                            throw(err);
+                        }
+
+                        uinput.key_event(stream, uinput.KEY_L, function(err) {
+                            if (err) {
+                                throw(err);
+                            }
+
+                            uinput.key_event(stream, uinput.KEY_O, function(err) {
+                                if (err) {
+                                    throw(err);
+                                }
+                            });
+                        });
+                    });
+                });
+            });
+        }, 1000);
+    });
+});
 
 
 /*
