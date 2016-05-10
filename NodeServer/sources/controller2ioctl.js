b67dae036bfb4315b31c3d0d48598a732bd8dcdf
Michael Schleiss <michaelschleiss87@gmail.com>
2016-05-10 18:11:03 +0200

    Uinput test5

--- NodeServer/sources/controller2ioctl.js
+++ NodeServer/sources/controller2ioctl.js
@@ -6,7 +6,7 @@ var uinput = require('uinput');
 var setup_options = {
     EV_KEY : [ uinput.KEY_H, uinput.KEY_E, uinput.KEY_L, uinput.KEY_O ]
 }
-var hellotest = function() {
+exports.hellotest = function() {
     uinput.setup(setup_options, function (err, stream) {
         if (err) {
             throw(err);
