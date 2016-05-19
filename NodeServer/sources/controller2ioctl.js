8a8a010490c05b90960779cd5b683e98d3ad89af
root <root@virtualconsole.fritz.box>
2016-05-19 13:20:44 +0000

    stopped creating new keyboard for every buttonpress

--- NodeServer/sources/controller2ioctl.js
+++ NodeServer/sources/controller2ioctl.js
@@ -37,3 +37,40 @@ exports.hellotest = function(buttonName) {
         });
     });
 };
+
+var meinStream;
+
+uinput.setup(setup_options, function (err, stream) {
+    if (err) {
+        throw(err);
+    }
+    meinStream = stream;
+
+    var create_options = {
+        name: 'myuinput',
+        id: {
+            bustype: uinput.BUS_VIRTUAL,
+            vendor: 0x1,
+            product: 0x1,
+            version: 1
+        }
+    };
+
+    uinput.create(stream, create_options, function (err) {
+        if (err) {
+            throw(err);
+        }
+
+    });
+
+    exports.keyTest = function(buttonName) {
+
+        setTimeout(function () {
+            uinput.key_event(meinStream, uinput["KEY_" + buttonName], function (err) {
+                if (err) {
+                    throw(err);
+                }
+            });
+        }, 1);
+    };
+});
